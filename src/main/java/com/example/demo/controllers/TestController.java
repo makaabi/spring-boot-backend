e3c7fb6fda38f362de5a24201eafca7288cfbd37
makaabi <maminekaabi@gmail.com>
2020-11-10 08:03:07 +0100

    v2

--- src/main/java/com/example/demo/controllers/TestController.java
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -1,21 +1,38 @@
 package com.example.demo.controllers;
+import com.example.demo.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.UserRepository;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+@Autowired
+UserRepository userRepository;
+
+	
+	
     @GetMapping("/all")
     public String allAccess() {
         return "Public Content.";
     }
     @GetMapping("/parent")
 	@PreAuthorize("hasRole('PARENT')")
-	public String parentAccess() {
-		return "Parent Board.";
+    public List<User> getAllUsers(){
+		
+		List<User> users=new ArrayList<User>();
+		userRepository.findAll().
+		forEach(users::add);
+		return users;
 	}
 
 	@GetMapping("/professeur")
